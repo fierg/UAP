3ee275f340e74cba3b1138cdadb4df6e78017588
Sven Fiergolla <sven.fiergolla@gmail.com>
2022-01-27 10:25:07 +0100

    Added args handling
    - added https://github.com/Kotlin/kotlinx-cli but only supports double dash as arg seperator
    - minor changes

--- triplac/src/main/kotlin/uap/export/DOTWriter.kt
+++ triplac/src/main/kotlin/uap/export/DOTWriter.kt
@@ -1,20 +1,32 @@
 package uap.export
 
 import org.jgrapht.graph.SimpleDirectedGraph
+import org.jgrapht.nio.Attribute
+import org.jgrapht.nio.DefaultAttribute
 import org.jgrapht.nio.dot.DOTExporter
 import uap.cfg.CFGNode
 import uap.cfg.Edge
 import java.io.StringWriter
+
 
 class DOTWriter {
     companion object {
 
         fun exportGraph(graph: SimpleDirectedGraph<CFGNode, Edge>) {
             //Create the exporter (with ID provider)
-            val exporter2 =
-                DOTExporter<CFGNode, Edge> { v: CFGNode -> "${v.node.type}${v.node.hashCode()}" }
+            val exporter = DOTExporter<CFGNode, Edge>()
+            exporter.setVertexAttributeProvider { v ->
+                val map: MutableMap<String, Attribute> = LinkedHashMap()
+                map["label"] = DefaultAttribute.createAttribute("${v.node.type}${v.node.hashCode()}")
+                map
+            }
+            exporter.setEdgeAttributeProvider { e ->
+                val map: MutableMap<String, Attribute> = LinkedHashMap()
+                map["label"] = DefaultAttribute.createAttribute(e.label)
+                map
+            }
             val writer = StringWriter()
-            exporter2.exportGraph(graph, writer)
+            exporter.exportGraph(graph, writer)
             println(writer.toString())
         }
     }
